a8289a2d57a8ef24af742e21423e312b62b85ee6
XardecQs <126134158+XardecQs@users.noreply.github.com>
2025-08-14 18:24:43 -0500

    jue 14 ago 2025 18:24

--- modules/users/xardec/files.nix
+++ modules/users/xardec/files.nix
@@ -10,7 +10,7 @@
     ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/nvim";
     ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/kitty";
     ".config/fastfetch".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/fastfetch";
-    ".vscode".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/vscode";
+    ".vscode".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/vscode";
     ".icons".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/icons";
     ".local/share/fonts".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/fonts";
     ".config/zsh".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/zsh";
